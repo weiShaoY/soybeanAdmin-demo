adb34d6045553b788a37e5944a13b01440f19c6c
weiShaoY_ <1604705673@qq.com>
2025-03-17 22:44:46 +0800

    chore: 🔧添加注释

--- src/store/modules/theme/shared.ts
+++ src/store/modules/theme/shared.ts
@@ -24,19 +24,31 @@ export function initThemeSettings() {
 
   // 如果是生产模式，主题设置将被缓存到 localStorage 中
   // 如果想在发布新版本时更新主题设置，请更新 `src/theme/settings.ts` 中的 `overrideThemeSettings`
+  /**
+   *  从本地存储中获取主题设置
+   */
+  const localSettings = localStg.get('themeSettings')
 
-  const localSettings = localStg.get('themeSettings') // 从本地存储中获取主题设置
+  /**
+   *  合并本地设置和默认主题设置
+   */
+  let settings = defu(localSettings, themeSettings)
 
-  let settings = defu(localSettings, themeSettings) // 合并本地设置和默认主题设置
-
-  const isOverride = localStg.get('overrideThemeFlag') === BUILD_TIME // 判断是否需要覆盖主题设置
+  /**
+   *  判断是否需要覆盖主题设置
+   */
+  const isOverride = localStg.get('overrideThemeFlag') === BUILD_TIME
 
   if (!isOverride) {
-    settings = defu(overrideThemeSettings, settings) // 合并覆盖主题设置
-    localStg.set('overrideThemeFlag', BUILD_TIME) // 设置覆盖标志
+    // 合并覆盖主题设置
+    settings = defu(overrideThemeSettings, settings)
+
+    // 设置覆盖标志
+    localStg.set('overrideThemeFlag', BUILD_TIME)
   }
 
-  return settings // 返回最终的主题设置
+  // 返回最终的主题设置
+  return settings
 }
 
 /**
@@ -50,10 +62,17 @@ export function createThemeToken(
   tokens?: App.Theme.ThemeSetting['tokens'],
   recommended = false,
 ) {
-  const paletteColors = createThemePaletteColors(colors, recommended) // 创建主题调色板颜色
+  /**
+   *  创建主题调色板颜色
+   */
+  const paletteColors = createThemePaletteColors(colors, recommended)
 
-  const { light, dark } = tokens || themeSettings.tokens // 获取主题令牌，默认为默认主题设置中的令牌
+  // 获取主题令牌，默认为默认主题设置中的令牌
+  const { light, dark } = tokens || themeSettings.tokens
 
+  /**
+   *   亮色主题令牌
+   */
   const themeTokens: App.Theme.ThemeTokenCSSVars = {
     colors: {
       ...paletteColors, // 调色板颜色
@@ -65,19 +84,35 @@ export function createThemeToken(
     },
   }
 
+  /**
+   *   暗色主题令牌
+   */
   const darkThemeTokens: App.Theme.ThemeTokenCSSVars = {
     colors: {
-      ...themeTokens.colors, // 继承亮色主题颜色
-      ...dark?.colors, // 暗色主题颜色
+      // 继承亮色主题颜色
+      ...themeTokens.colors,
+
+      // 暗色主题颜色
+      ...dark?.colors,
     },
     boxShadow: {
-      ...themeTokens.boxShadow, // 继承亮色主题阴影
-      ...dark?.boxShadow, // 暗色主题阴影
+      // 继承亮色主题阴影
+      ...themeTokens.boxShadow,
+
+      // 暗色主题阴影
+      ...dark?.boxShadow,
     },
   }
 
   return {
+    /**
+     *  亮色主题令牌
+     */
     themeTokens,
+
+    /**
+     *  暗色主题令牌
+     */
     darkThemeTokens,
   }
 }
@@ -89,22 +124,34 @@ export function createThemeToken(
  * @param [recommended] 是否使用推荐颜色。默认值为 `false`. Default is `false`
  */
 function createThemePaletteColors(colors: App.Theme.ThemeColor, recommended = false) {
-  const colorKeys = Object.keys(colors) as App.Theme.ThemeColorKey[] // 获取颜色键
+  /**
+   *  获取颜色键
+   */
+  const colorKeys = Object.keys(colors) as App.Theme.ThemeColorKey[]
 
+  /**
+   *  初始化调色板颜色变量
+   */
   const colorPaletteVar = {
-  } as App.Theme.ThemePaletteColor // 初始化调色板颜色变量
+  } as App.Theme.ThemePaletteColor
 
   colorKeys.forEach((key) => {
-    const colorMap = getColorPalette(colors[key], recommended) // 获取颜色调色板
+    /**
+     *  获取颜色调色板
+     */
+    const colorMap = getColorPalette(colors[key], recommended)
 
-    colorPaletteVar[key] = colorMap.get(500)! // 设置主色调
+    // 设置主色调
+    colorPaletteVar[key] = colorMap.get(500)!
 
     colorMap.forEach((hex, number) => {
-      colorPaletteVar[`${key}-${number}`] = hex // 设置不同色阶的颜色
+      // 设置不同色阶的颜色
+      colorPaletteVar[`${key}-${number}`] = hex
     })
   })
 
-  return colorPaletteVar // 返回调色板颜色
+  // 返回调色板颜色
+  return colorPaletteVar
 }
 
 /**
@@ -112,36 +159,60 @@ function createThemePaletteColors(colors: App.Theme.ThemeColor, recommended = fa
  * @param tokens 主题基础令牌
  */
 function getCssVarByTokens(tokens: App.Theme.BaseToken) {
-  const styles: string[] = [] // 初始化样式数组
+  /**
+   *  初始化样式数组
+   */
+  const styles: string[] = []
 
+  /**
+   *   移除 CSS 变量前缀
+   */
   function removeVarPrefix(value: string) {
-    return value.replace('var(', '').replace(')', '') // 移除 CSS 变量前缀
+    return value.replace('var(', '').replace(')', '')
   }
 
+  /**
+   *  移除 RGB 前缀
+   */
   function removeRgbPrefix(value: string) {
-    return value.replace('rgb(', '').replace(')', '') // 移除 RGB 前缀
+    return value.replace('rgb(', '').replace(')', '')
   }
 
   for (const [key, tokenValues] of Object.entries(themeVars)) {
     for (const [tokenKey, tokenValue] of Object.entries(tokenValues)) {
-      let cssVarsKey = removeVarPrefix(tokenValue) // 获取 CSS 变量键
+      /**
+       *  获取 CSS 变量键
+       */
+      let cssVarsKey = removeVarPrefix(tokenValue)
 
-      let cssValue = tokens[key][tokenKey] // 获取 CSS 变量值
+      /**
+       *  获取 CSS 变量值
+       */
+      let cssValue = tokens[key][tokenKey]
 
       if (key === 'colors') {
-        cssVarsKey = removeRgbPrefix(cssVarsKey) // 如果是颜色，移除 RGB 前缀
-        const { r, g, b } = getRgb(cssValue) // 获取 RGB 值
+        // 如果是颜色，移除 RGB 前缀
+        cssVarsKey = removeRgbPrefix(cssVarsKey)
 
-        cssValue = `${r} ${g} ${b}` // 设置 RGB 格式的值
+        // 获取 RGB 值
+        const { r, g, b } = getRgb(cssValue)
+
+        // 设置 RGB 格式的值
+        cssValue = `${r} ${g} ${b}`
       }
 
-      styles.push(`${cssVarsKey}: ${cssValue}`) // 将 CSS 变量添加到样式数组
+      // 将 CSS 变量添加到样式数组
+      styles.push(`${cssVarsKey}: ${cssValue}`)
     }
   }
 
-  const styleStr = styles.join(';') // 将样式数组拼接成字符串
+  /**
+   *  将样式数组拼接成字符串
+   */
+  const styleStr = styles.join(';')
 
-  return styleStr // 返回样式字符串
+  // 返回样式字符串
+  return styleStr
 }
 
 /**
@@ -151,23 +222,44 @@ function getCssVarByTokens(tokens: App.Theme.BaseToken) {
  * @param darkTokens 暗黑主题令牌
  */
 export function addThemeVarsToGlobal(tokens: App.Theme.BaseToken, darkTokens: App.Theme.BaseToken) {
-  const cssVarStr = getCssVarByTokens(tokens) // 获取亮色主题的 CSS 变量字符串
+  /**
+   *  获取亮色主题的 CSS 变量字符串
+   */
+  const cssVarStr = getCssVarByTokens(tokens)
 
-  const darkCssVarStr = getCssVarByTokens(darkTokens) // 获取暗黑主题的 CSS 变量字符串
+  /**
+   *  获取暗黑主题的 CSS 变量字符串
+   */
+  const darkCssVarStr = getCssVarByTokens(darkTokens)
 
-  const css = `:root { ${cssVarStr} }` // 创建亮色主题的 CSS
+  /**
+   *  创建亮色主题的 CSS
+   */
+  const css = `:root { ${cssVarStr} }`
 
-  const darkCss = `html.${DARK_CLASS} { ${darkCssVarStr} }` // 创建暗黑主题的 CSS
+  /**
+   *  创建暗黑主题的 CSS
+   */
+  const darkCss = `html.${DARK_CLASS} { ${darkCssVarStr} }`
 
-  const styleId = 'theme-vars' // 样式 ID
+  /**
+   *  样式 ID
+   */
+  const styleId = 'theme-vars'
 
-  const style = document.querySelector(`#${styleId}`) || document.createElement('style') // 获取或创建样式元素
+  /**
+   *  获取样式元素
+   */
+  const style = document.querySelector(`#${styleId}`) || document.createElement('style')
 
-  style.id = styleId // 设置样式 ID
+  // 设置样式 ID
+  style.id = styleId
 
-  style.textContent = css + darkCss // 设置样式内容
+  // 设置样式内容
+  style.textContent = css + darkCss
 
-  document.head.appendChild(style) // 将样式添加到文档头部
+  // 将样式添加到文档头部
+  document.head.appendChild(style)
 }
 
 /**
@@ -175,13 +267,16 @@ export function addThemeVarsToGlobal(tokens: App.Theme.BaseToken, darkTokens: Ap
  * @param darkMode 是否为暗模式
  */
 export function toggleCssDarkMode(darkMode = false) {
-  const { add, remove } = toggleHtmlClass(DARK_CLASS) // 获取切换 HTML 类名的函数
+  // 获取切换 HTML 类名的函数
+  const { add, remove } = toggleHtmlClass(DARK_CLASS)
 
   if (darkMode) {
-    add() // 添加暗黑模式类名
+    // 添加暗黑模式类名
+    add()
   }
   else {
-    remove() // 移除暗黑模式类名
+    // 移除暗黑模式类名
+    remove()
   }
 }
 
@@ -191,9 +286,13 @@ export function toggleCssDarkMode(darkMode = false) {
  * @param colourWeakness 色盲模式
  */
 export function toggleAuxiliaryColorModes(grayscaleMode = false, colourWeakness = false) {
-  const htmlElement = document.documentElement // 获取 HTML 元素
+  /**
+   *  获取 HTML 元素
+   */
+  const htmlElement = document.documentElement
 
+  // 设置滤镜效果
   htmlElement.style.filter = [grayscaleMode ? 'grayscale(100%)' : '', colourWeakness ? 'invert(80%)' : '']
     .filter(Boolean)
-    .join(' ') // 设置滤镜效果
+    .join(' ')
 }
