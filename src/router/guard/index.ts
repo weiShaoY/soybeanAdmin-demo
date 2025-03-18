68e3e4211f9f8055fd3ce4f7312cad0f6cb8351a
weiShaoY_ <1604705673@qq.com>
2025-03-18 18:03:46 +0800

    chore: 🔧调整路由拦截函数位置

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -2,7 +2,7 @@ import type { Router } from 'vue-router'
 
 import { useTitle } from '@vueuse/core'
 
-import { createRouteGuard } from './routeGuard'
+import { createRouteGuard } from './route'
 
 /**
  * 创建进度条守卫
--- src/router/utils/index.ts
+++ src/router/utils/index.ts
@@ -1,3 +1,1 @@
 export * from './getVueRouteTemplate'
-
-export * from './guard'
