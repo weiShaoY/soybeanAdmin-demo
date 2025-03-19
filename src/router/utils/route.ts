2def0eb10f1fe713ca3f8750cd4b80d5c110cb5e
weiShaoY_ <1604705673@qq.com>
2025-03-19 21:10:03 +0800

    chore: 🔧优化sortRoutesByOrder

--- src/router/utils/route.ts
+++ src/router/utils/route.ts
@@ -1,16 +1,13 @@
 import type { ElegantConstRoute } from '@elegant-router/types'
 
 /**
- * 根据顺序排序路由
- * @param  route 路由
- * @returns  排序后的路由
+ * 递归排序路由及其子路由
+ * @param route 路由
+ * @returns 排序后的路由
  */
-function sortRouteByOrder(route: ElegantConstRoute) {
+function sortRouteByOrder(route: ElegantConstRoute): ElegantConstRoute {
   if (route.children?.length) {
-    route.children.sort(
-      (next, prev) =>
-        (Number(next.meta?.order) || 0) - (Number(prev.meta?.order) || 0),
-    )
+    route.children.sort((a, b) => (a.meta?.order || 0) - (b.meta?.order || 0))
     route.children.forEach(sortRouteByOrder)
   }
 
@@ -19,15 +16,11 @@ function sortRouteByOrder(route: ElegantConstRoute) {
 
 /**
  * 根据顺序排序路由数组
- * @param  routes 路由数组
- * @returns  排序后的路由数组
+ * @param routes 路由数组
+ * @returns 排序后的路由数组
  */
-export function sortRoutesByOrder(routes: ElegantConstRoute[]) {
-  routes.sort(
-    (next, prev) =>
-      (Number(next.meta?.order) || 0) - (Number(prev.meta?.order) || 0),
-  )
-  routes.forEach(sortRouteByOrder)
-
+export function sortRoutesByOrder(routes: ElegantConstRoute[]): ElegantConstRoute[] {
   return routes
+    .sort((a, b) => (a.meta?.order || 0) - (b.meta?.order || 0))
+    .map(sortRouteByOrder)
 }
